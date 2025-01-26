dfe9e16f6903364585db9e84d331ab701c8a5d38
CERTIFIED_CLOWN <rymbenhmida2005@gmail.com>
2025-01-26 13:13:54 +0100

    initial commit

---
+++ contact.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const form = document.querySelector('#contact-form');
+    
+    form.addEventListener('submit', (e) => {
+
+        const name=document.getElementById("name").value.trim();
+        const email=document.getElementById("email").value.trim();
+        const message=document.getElementById("message").value.trim();
+
+    if(!name){
+        alert("Please enter your full name (╥﹏╥)");
+        e.preventDefault();
+        return;
+    }
+
+    if(!email){
+        alert("Please enter your email (╥﹏╥)");
+        e.preventDefault();
+        return;
+    }
+
+    if(!message){
+        alert("Please enter your message (╥﹏╥)");
+        e.preventDefault();
+        return;
+    }
+    alert('Your message is sent◝(ᵔᗜᵔ)◜');
+    e.preventDefault();
+    });
+});
